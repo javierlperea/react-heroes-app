f3b477c87c9eba400b509020b4775b19cc478d16
Javier Perea <javierlperea@gmail.com>
2021-01-22 08:33:00 -0300

    actualizado

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -64,7 +64,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
 
                     <span className="nav-item nav-link text-info"> 
-                        { name }
+                        Javier
                     </span>
 
                     <button 
